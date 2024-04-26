e4755c915e874d51f34884bfb9fda68c52a94633
alessandro.bartsch@jv01.local <alessandro.bartsch@totvs.com.br>
2024-04-26 09:56:12 -0300

    base project

---
+++ src/main/java/com/x/bugui/BuguiApplication.java
@@ -0,0 +1,13 @@
+package com.x.bugui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuguiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/x/bugui/BuguiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x.bugui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
